66e17f813bbc8802628dfa16acc42a73141a7603
MrF1ow <eflow1280@gmail.com>
2024-08-06 20:02:55 -0700

    Implemented Redux and Redux-Persist
    I implemented part of Redux for authentication. If you do not know what Redux is, please look into it same goes for redux-persist. These are going to be helpful
    us in the future.

---
+++ src/redux/store.ts
@@ -0,0 +1,47 @@
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { useDispatch, TypedUseSelectorHook, useSelector } from "react-redux";
+import { persistReducer } from "redux-persist";
+import { authReducer } from "./slices/authSlice";
+import createWebStorage from "redux-persist/lib/storage/createWebStorage";
+
+const createNoopStorage = () => {
+  return {
+    getItem() {
+      return Promise.resolve(null);
+    },
+    setItem(_key: string, value: number) {
+      return Promise.resolve(value);
+    },
+    removeItem() {
+      return Promise.resolve();
+    },
+  };
+};
+
+const storage =
+  typeof window !== "undefined"
+    ? createWebStorage("local")
+    : createNoopStorage();
+
+const authPersistConfig = {
+  key: "auth",
+  storage: storage,
+  whitelist: ["authState"],
+};
+
+const rootReducer = combineReducers({
+  auth: persistReducer(authPersistConfig, authReducer),
+});
+
+export const store = configureStore({
+  reducer: rootReducer,
+  middleware: (
+    getDefaultMiddleware: (arg0: { serializableCheck: boolean }) => any
+  ) => getDefaultMiddleware({ serializableCheck: false }),
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
